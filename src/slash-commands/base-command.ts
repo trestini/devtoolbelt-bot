9d40b3b6319d870042fb53f364ad0f81cbebffd4
Rafael Trestini <trestini@gmail.com>
2021-10-17 18:01:17 -0300

    feat: support for quoted parameters

--- src/slash-commands/base-command.ts
+++ src/slash-commands/base-command.ts
@@ -1,34 +1,39 @@
-import { BasicResponse, BlockKitResponse, SlackSlashCommand, SlackSlashCommandPayload } from "./types";
+import {
+  BasicResponse,
+  BlockKitResponse,
+  SlackSlashCommand,
+  SlackSlashCommandPayload,
+} from "./types";
+
+import { splitSpacesExcludeQuotes as splitter } from "quoted-string-space-split";
 
 type Command = {
-  command: String
-  utility: String
-  times?: number
-  args?: Array<string>
-}
+  command: String;
+  utility: String;
+  times?: number;
+  args?: Array<string>;
+};
 
 export abstract class BaseCommand implements SlackSlashCommand {
-
   protected parse(payload: SlackSlashCommandPayload): Command {
-
-    const {command, text} = payload;
-    const [utility, ...params] = text.split(" ");
+    const { command, text } = payload;
+    const [utility, ...params] = splitter(text)
 
     return {
       command,
       utility,
       times: this.findTimes(params),
-      args: params
-    }
-
+      args: params,
+    };
   }
 
   private findTimes(args: Array<string>): number {
-    const x = args.filter(arg => arg.charAt(0) === "x");
+    const x = args.filter((arg) => arg.charAt(0) === "x");
     return x.length > 0 ? parseInt(x[0].replace("x", "")) : 1;
   }
 
-  abstract get routes(): Array<string>
-  abstract process(payload: SlackSlashCommandPayload): Promise<BasicResponse | BlockKitResponse>
-
+  abstract get routes(): Array<string>;
+  abstract process(
+    payload: SlackSlashCommandPayload
+  ): Promise<BasicResponse | BlockKitResponse>;
 }
